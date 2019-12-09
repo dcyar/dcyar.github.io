ec9944e9f4fbae7f14890f650a60dd4026b64f60
Jhon Achata Limachi <jhonal183@gmail.com>
2019-12-08 23:39:41 -0500

    add latest projects, posts and new project

---
+++ src/content/project/25-11-2019-bookstore/bookstore.md
@@ -0,0 +1,34 @@
+---
+path: "/laravel-bookstore"
+title: "Laravel Bookstore"
+date: 2019-12-08
+thumbnail: "../thumbnails/default.png"
+group: "project"
+category: "development"
+description: 'Laravel Bookstore es un ecommerce pequeño, donde se muestra lo basico para una tienda online.'
+tags:
+    - open source
+    - laravel
+    - ecommerce
+---
+
+**<a href="https://github.com/dcyar/bookstore" target="_blank">Laravel Bookstore</a>** es un proyecto Open Source de ecommerce con lo basico de la funcionalidad que podria tener una tienda en linea.
+
+![OREC-EX Dashboard](./backend.png)
+
+**Caracteristicas del Sistema:**
+- Panel de administracion (Usuarios, Roles, Planes, Autores, Libros).
+- Cartera de usuario.
+- Biblioteca de usuario.
+
+*Les dejo algunas capturas del sistema.*
+
+![OREC-EX Login](./frontend.png)
+
+![OREC-EX Login](./new-book.png)
+
+![OREC-EX Login](./wallet.png)
+
+Si deseas usar el sistema y necesitas ayuda con la instalacion o el uso, puedes contactarme al siguiente email: ***jachata.m@gmail.com***
+
+> Puedes acceder a una copia del sistema desde aqui <a href="https://github.com/dcyar/bookstore" target="_blank">Laravel Bookstore</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,65 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../layout/layout"
 import SEO from "../components/shared/seo"
 import Vcard from "../components/vcard"
 
 import siteConfig from "../../data/siteConfig"
+import Feed from "../components/feed"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
     <Layout config={siteConfig}>
         <SEO title="Home" />
-        <Vcard/>
+        <Vcard />
+        <Feed data={data.projects.edges} title="Latest Projects" path="/project" />
+        <Feed data={data.posts.edges} title="Latest Post" path="/blog" />
     </Layout>
 )
 
 export default IndexPage
+
+export const data = graphql`
+        query FeedProjectsAndFeedPosts {
+            projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, filter: {frontmatter: {group: {eq: "project"}}}, limit: 10){
+                edges {
+                    node {
+                        frontmatter {
+                            path
+                            title
+                            description
+                            date
+                            tags
+                            thumbnail {
+                                childImageSharp {
+                                    fixed(width: 35, height: 35) {
+                                    ...GatsbyImageSharpFixed
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            posts: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, filter: {frontmatter: {group: {eq: "blog"}}}, limit: 10){
+                edges {
+                    node {
+                        frontmatter {
+                            path
+                            title
+                            description
+                            date
+                            tags
+                            thumbnail {
+                                childImageSharp {
+                                    fixed(width: 35, height: 35) {
+                                    ...GatsbyImageSharpFixed
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `
